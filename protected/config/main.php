0c7cc53b9ea3ac4d4c342e3f6554f100221037b6
sanchay <snlgrglns@gmail.com>
2020-10-11 23:49:06 +1100

    setting change

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Hotel Name',
+	'name'=>'Hotel Name ',
     'sourceLanguage'=>'en',
 
 	// preloading 'log' component
